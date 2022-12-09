aff30e168ee98b43e5809c292201cda093bece97
conilov <89157156+conilov@users.noreply.github.com>
2022-12-09 16:16:23 -0300

    Add files via upload

---
+++ data/AtractivosCulturales_2.js
@@ -0,0 +1,13 @@
+var json_AtractivosCulturales_2 = {
+"type": "FeatureCollection",
+"name": "AtractivosCulturales_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Nombre": "Centro Cultural Casa Blanca" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ -71.411114374402231, -33.32083690378316 ] ] } },
+{ "type": "Feature", "properties": { "Nombre": "Ex ballenera" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ -71.702263456468117, -33.192602850996821 ] ] } },
+{ "type": "Feature", "properties": { "Nombre": "Fundacion Museo Quintay" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ -71.702479526209032, -33.193003878929062 ] ] } },
+{ "type": "Feature", "properties": { "Nombre": "Museo Julio Munizaga" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ -71.697066501961345, -33.192030426112552 ] ] } },
+{ "type": "Feature", "properties": { "Nombre": "Observatorio Toki Kura" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ -71.444387320239045, -33.310466378223801 ] ] } },
+{ "type": "Feature", "properties": { "Nombre": "Santuario Lo Vasquez" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ -71.438874299194026, -33.256700033312313 ] ] } }
+]
+}
---
+++ data/RutadelVino_4.js
@@ -0,0 +1,18 @@
+var json_RutadelVino_4 = {
+"type": "FeatureCollection",
+"name": "RutadelVino_4",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Name": "House Casa del Vino" }, "geometry": { "type": "Point", "coordinates": [ -71.318203058789123, -33.361045574122734 ] } },
+{ "type": "Feature", "properties": { "Name": "Viña Catrala" }, "geometry": { "type": "Point", "coordinates": [ -71.376181462920343, -33.19739288080531 ] } },
+{ "type": "Feature", "properties": { "Name": "Viña Emiliana" }, "geometry": { "type": "Point", "coordinates": [ -71.312474512162638, -33.364651024768015 ] } },
+{ "type": "Feature", "properties": { "Name": "Viña Estancia El Cuadro" }, "geometry": { "type": "Point", "coordinates": [ -71.287429183435819, -33.266955659894606 ] } },
+{ "type": "Feature", "properties": { "Name": "Viña Indómita" }, "geometry": { "type": "Point", "coordinates": [ -71.343859104835317, -33.359707038182613 ] } },
+{ "type": "Feature", "properties": { "Name": "Viña Kingston" }, "geometry": { "type": "Point", "coordinates": [ -71.460530666295767, -33.317072307507409 ] } },
+{ "type": "Feature", "properties": { "Name": "Viña Loma Larga" }, "geometry": { "type": "Point", "coordinates": [ -71.385496403510658, -33.288411363475184 ] } },
+{ "type": "Feature", "properties": { "Name": "Viña Mar" }, "geometry": { "type": "Point", "coordinates": [ -71.353087261828733, -33.358441360512479 ] } },
+{ "type": "Feature", "properties": { "Name": "Viña Quintay" }, "geometry": { "type": "Point", "coordinates": [ -71.330068117981227, -33.355891117232716 ] } },
+{ "type": "Feature", "properties": { "Name": "Viña Veramonte" }, "geometry": { "type": "Point", "coordinates": [ -71.294497997285333, -33.370016196791923 ] } },
+{ "type": "Feature", "properties": { "Name": "Viña William Cole" }, "geometry": { "type": "Point", "coordinates": [ -71.348918031999133, -33.31683538066487 ] } }
+]
+}
